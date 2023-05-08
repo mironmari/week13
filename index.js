70467240321aa057da89bcebdddb8eb4dee2b348
[mironmari] <[mironrnko.mv@mail.ru>
2023-05-08 01:09:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+alert('test!');
+
+let salary1 = 40000;
+let salary2 = 22000;
+alert(salary1 + salary2);
+
+let name1 = ("Владивосток");
+let nomber = 2000;
+let song = `${name1} ${nomber}`;
+alert(song);
+
+let chat = 128;
+let like = 254;
+let vk = 137;
+let youtube = 201;
+alert(chat + like + vk + youtube);
+
+let a = 2;
+let b = 5;
+let c = a ** b;
+alert(c);
+
+let text = ('"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"');
+alert(text);
+
+const greetings = "Привет";
+const name2 = "Кот";
+let hello = `"${greetings}, ${name2}"`;
+alert(hello);
+
+let time;
+time = 34;
+alert(time);
+console.log(time);
+console.log('Старт поездки. Осталось минут: ' + time);
+let time1 = 15;
+let finish = time - time1;
+console.log('Осталось минут: ' + finish);
+console.log('Немного сториз в соцсетях. Осталось минут: ' + finish);
+let time2 = 15 + 10;
+let finish1 = time - time2;
+console.log('Немного не новостей, но событий. Осталось минут: ' + finish1);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву ');
+
+
+let fahrenheit;
+fahrenheit = 451;
+let celsius;
+celsius = (fahrenheit - 32) / 1.8;
+console.log(celsius);
+console.log('Градус по Фаренгейту: ' + fahrenheit , 'Градус по Цельсию: ' + celsius);
+
+
